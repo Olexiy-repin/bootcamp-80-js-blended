a641304ec97ac397c57e2b9661f8bc6ca8332902
Olexiy-repin <repinlyoha@gmail.com>
2026-04-28 17:21:14 +0300

    classwork

--- src/js/tasks-api.js
+++ src/js/tasks-api.js
@@ -1,9 +1,16 @@
 //TODO: BaseURL(https://69f06c1fc1533dbedc9cde50.mockapi.io)
-
 import axios from 'axios';
 
-export const getAllTasks = () => {};
+axios.defaults.baseURL = 'https://69f06c1fc1533dbedc9cde50.mockapi.io';
 
-export const createNewTask = taskText => {};
+export const getAllTasks = () => {
+  return axios.get('/tasks');
+};
 
-export const deleteTaskById = taskId => {};
+export const createNewTask = task => {
+  return axios.post('/tasks', task);
+};
+
+export const deleteTaskById = taskId => {
+  return axios.delete(`/tasks/${taskId}`);
+};
