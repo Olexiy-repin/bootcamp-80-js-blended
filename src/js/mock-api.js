097d0907157136265207b9413c9db4a8ec66c492
Olexiy-repin <repinlyoha@gmail.com>
2026-04-30 17:18:58 +0300

    classwork

--- src/js/mock-api.js
+++ src/js/mock-api.js
@@ -1,13 +1,21 @@
 import axios from 'axios';
 
-export const getAllPosts = () => {
-  return axios.get('https://69f06c1fc1533dbedc9cde50.mockapi.io/posts');
+axios.defaults.baseURL = 'https://69f06c1fc1533dbedc9cde50.mockapi.io';
+
+export const getAllPosts = async () => {
+  const { data } = await axios.get('/posts');
+
+  return data;
 };
 
-export const createNewPost = post => {
-  return axios.post('https://69f06c1fc1533dbedc9cde50.mockapi.io/posts', post);
+export const createNewPost = async post => {
+  const { data } = await axios.post('/posts', post);
+
+  return data;
 };
 
-export const deletePostById = postId => {
-  return axios.delete(`https://69f06c1fc1533dbedc9cde50.mockapi.io/posts/${postId}`);
+export const deletePostById = async postId => {
+  const { data } = await axios.delete(`/posts/${postId}`);
+
+  return data;
 };
--- src/js/posts.js
+++ src/js/posts.js
@@ -6,69 +6,69 @@ const refs = {
   postForm: document.querySelector('.js-post-form'),
 };
 
-const renderPosts = () => {
-  getAllPosts()
-    .then(({ data: postsArr }) => {
-      const postCardsTemplate = postsArr.map(post => createPostCardTemplate(post)).join('');
+const renderPosts = async () => {
+  try {
+    const postsArr = await getAllPosts();
 
-      refs.postsList.innerHTML = postCardsTemplate;
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    const postCardsTemplate = postsArr.map(post => createPostCardTemplate(post)).join('');
+
+    refs.postsList.innerHTML = postCardsTemplate;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 renderPosts();
 
-const onPostFormSubmit = event => {
-  event.preventDefault();
+const onPostFormSubmit = async event => {
+  try {
+    event.preventDefault();
 
-  const { target: postFormEl } = event;
+    const { target: postFormEl } = event;
 
-  const newPost = {
-    post_title: postFormEl.elements.post_title.value.trim(),
-    post_body: postFormEl.elements.post_body.value.trim(),
-  };
+    const newPost = {
+      post_title: postFormEl.elements.post_title.value.trim(),
+      post_body: postFormEl.elements.post_body.value.trim(),
+    };
 
-  const newPostValues = Object.values(newPost);
+    const newPostValues = Object.values(newPost);
 
-  if (newPostValues.includes('')) {
-    alert('Усі поля мають бути заповнені!');
+    if (newPostValues.includes('')) {
+      alert('Усі поля мають бути заповнені!');
 
-    return;
+      return;
+    }
+
+    const post = await createNewPost(newPost);
+
+    alert(`${post.post_title} додано успішно!`);
+
+    postFormEl.reset();
+
+    renderPosts();
+  } catch (error) {
+    console.log(err);
   }
-
-  createNewPost(newPost)
-    .then(({ data: post }) => {
-      alert(`${post.post_title} додано успішно!`);
-
-      postFormEl.reset();
-
-      renderPosts();
-    })
-    .catch(err => {
-      console.log(err);
-    });
 };
 
-const onPostDeleteBtnClick = event => {
-  const currentPostDeleteBtnEl = event.target.closest('.js-post-delete-btn');
+const onPostDeleteBtnClick = async event => {
+  try {
+    const currentPostDeleteBtnEl = event.target.closest('.js-post-delete-btn');
 
-  if (!currentPostDeleteBtnEl) {
-    return;
+    if (!currentPostDeleteBtnEl) {
+      return;
+    }
+
+    const currentPostId = currentPostDeleteBtnEl.dataset.postId;
+
+    const deletedPost = await deletePostById(currentPostId);
+
+    alert(`${deletedPost.post_title} видалено успішно!`);
+
+    renderPosts();
+  } catch (error) {
+    console.log(error);
   }
-
-  const currentPostId = currentPostDeleteBtnEl.dataset.postId;
-
-  deletePostById(currentPostId)
-    .then(({ data: deletedPost }) => {
-      alert(`${deletedPost.post_title} видалено успішно!`);
-
-      renderPosts();
-    })
-    .catch(err => {
-      console.log(err);
-    });
 };
 
 refs.postForm.addEventListener('submit', onPostFormSubmit);
